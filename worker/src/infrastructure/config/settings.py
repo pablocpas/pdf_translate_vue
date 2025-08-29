c028750c6e507cd615a201634bba4cd4cdddd682
Pablo Caño <62502825+pablocpas@users.noreply.github.com>
2025-08-29 18:11:32 +0200

    settings

--- worker/src/infrastructure/config/settings.py
+++ worker/src/infrastructure/config/settings.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     AWS_ACCESS_KEY_ID: str
     AWS_SECRET_ACCESS_KEY: str
     AWS_S3_BUCKET: str
-    AWS_REGION: Optional[str] = 'us-east-1'
+    AWS_REGION: Optional[str] = ''
     AWS_S3_USE_SSL: bool = False
     
     # --- Environment-specific Endpoints ---
